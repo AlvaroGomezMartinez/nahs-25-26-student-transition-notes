501f469adadafa2c49406a393f012be2dd7210de
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-09-19 18:00:59 -0500

    Worked on the rightJoin

--- sourceScript2.js
+++ sourceScript2.js
@@ -25,8 +25,6 @@ function getStudentsFromTENTATIVESheet() {
   return allStudentsMap;
 }
 
-
-
 /**
  * getStudentsFromEntryWithdrawalSheet() references the "Entry_Withdrawal" sheet of the "NAHS 24-25 Student Transition Notes" spreadsheet.
  * 
@@ -290,18 +288,17 @@ function findStudentIdByFullName(lastName, firstName, studentDataMap) {
   return null; // Return null if no match is found. Consider using regex in the future to account for name typos in the "W/D Other" sheet.
 }
 
-function registrationsData2() {
-  // Fetch the data from the respective sheets as Maps
-  let dataFromTENTATIVE = getStudentsFromTENTATIVESheet(); // Map with Student ID as key (objects as values)
-  let dataFromEntryWithdrawal = getStudentsFromEntryWithdrawalSheet(); // Map with Student ID as key (arrays as values)
 
-  // Performing a RIGHT JOIN between dataFromEntryWithdrawal and dataFromTENTATIVE
+function registrationsData2() {
+  let dataFromTENTATIVE = getStudentsFromTENTATIVESheet();
+  let dataFromEntryWithdrawal = getStudentsFromEntryWithdrawalSheet();
   let joinedData = rightJoin(dataFromEntryWithdrawal, dataFromTENTATIVE);
+  let dataFromFormResponses2 = getDataFromFormResponses2();
+  let secondJoinedData = leftJoin(joinedData, dataFromFormResponses2);
 
   // Now you can perform further operations on joinedData as needed
-  Logger.log("Joined Data: " + JSON.stringify([...joinedData]));
+  Logger.log("Second Joined Data: " + JSON.stringify([...secondJoinedData]));
 
-  // More logic to manipulate and use the joined data can be added here...
 }
 
 /**
@@ -422,6 +419,104 @@ function convertArrayToObject(array, headers) {
   return obj;
 }
 
+
+
+
+
+
+/**
+ * Function to perform a LEFT JOIN operation between two Maps.
+ * @param {Map} map1 - The left map where values are objects (joinedData)
+ * @param {Map} map2 - The right map where values are arrays (formResponses2 data)
+ * @returns {Map} - A new Map with the result of the LEFT JOIN.
+ */
+function leftJoin(map1, map2) {
+  let joinedMap = new Map();
+
+  // Array headers for dataFromEntryWithdrawal, replace with actual headers
+  let arrayHeaders = [
+    "DATE ADDED TO SPREADSHEET",
+    "LAST",
+    "FIRST",
+    "STUDENT ID",
+    "GRADE",
+    "1st Period - Course Title",
+    "1st Period - Teacher Name",
+    "1st Period - Transfer Grade",
+    "1st Period - Current Grade",
+    "1st Period - How would you assess this student's academic growth?",
+    "1st Period - Academic and Behavioral Progress Notes",
+    "2nd Period - Course Title",
+    "2nd Period - Teacher Name",
+    "2nd Period - Transfer Grade",
+    "2nd Period - Current Grade",
+    "2nd Period - How would you assess this student's academic progress?",
+    "2nd Period - Academic and Behavioral Progress Notes",
+    "3rd Period - Course Title",
+    "3rd Period - Teacher Name",
+    "3rd Period - Transfer Grade",
+    "3rd Period - Current Grade",
+    "3rd Period - How would you assess this student's academic progress?",
+    "3rd Period - Academic and Behavioral Progress Notes",
+    "4th Period - Course Title",
+    "4th Period - Teacher Name",
+    "4th Period - Transfer Grade",
+    "4th Period - Current Grade",
+    "4th Period - How would you assess this student's academic progress?",
+    "4th Period - Academic and Behavioral Progress Notes",
+    "5th Period - Course Title",
+    "5th Period - Teacher Name",
+    "5th Period - Transfer Grade",
+    "5th Period - Current Grade",
+    "5th Period - How would you assess this student's academic progress?",
+    "5th Period - Academic and Behavioral Progress Notes",
+    "6th Period - Course Title",
+    "6th Period - Teacher Name",
+    "6th Period - Transfer Grade",
+    "6th Period - Current Grade",
+    "6th Period - How would you assess this student's academic progress?",
+    "6th Period - Academic and Behavioral Progress Notes",
+    "7th Period - Course Title",
+    "7th Period - Teacher Name",
+    "7th Period - Transfer Grade",
+    "7th Period - Current Grade",
+    "7th Period - How would you assess this student's progress?",
+    "7th Period - Academic and Behavioral Progress Notes",
+    "8th Period - Course Title",
+    "8th Period - Teacher Name",
+    "8th Period - Transfer Grade",
+    "8th Period - Current Grade",
+    "8th Period - How would you assess this student's progress?",
+    "8th Period - Academic and Behavioral Progress Notes",
+    "SE - Special Education Case Manager",
+    "SE - What accommodations seem to work well with this student to help them be successful?",
+    "SE - What are the student's strengths, as far as behavior?",
+    "SE - What are the student's needs, as far as behavior?  (Pick behavior having most effect on his/her ability to be successful in class.  If there are no concerns, note that.)",
+    "SE - What are the student's needs, as far as functional skills?  (Include daily living skills, fine/gross motor skills, organizational skills, self-advocacy, attendance, etc.)",
+    "SE - Please add any other comments or concerns here:",
+    "REGULAR CAMPUS",
+    "FIRST DAY OF AEP",
+    "Anticipated Release Date",
+    "Parent Notice Date",
+    "Withdrawn Date",
+  ];
+
+  // Iterate over the entries in map1 (joinedData)
+  map1.forEach((data1, id) => {
+    let formData = map2.get(id); // convertArrayToObject(arrayData, arrayHeaders);
+
+    if (formData) {
+      // Merge the two objects giving precedence to the data1
+      let mergedData = { ...formData, ...data1 };
+      joinedMap.set(id, mergedData);
+    } else {
+      // If no matching ID in map2, just use the data1
+      joinedMap.set(id, data1);
+    }
+  });
+
+  return joinedMap;
+}
 
 // Run the main function
 // registrationsData2();
