8eb96ff177de5e43d58335dbf3ab2dbc94332e47
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-10-02 08:20:25 -0500

    Updated the "if" statement so that all of the data is pushed to the map (includes rows with repeating student ids)

--- loadTentativeData.js
+++ loadTentativeData.js
@@ -4,9 +4,7 @@
  * @returns {allStudentsMap1} A map where the key is the Student ID the values are an object containing student data from the rows in TENTATIVE.
  */
 function getStudentsFromTENTATIVESheet() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
-    "TENTATIVE2(TESTING)",
-  );
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("TENTATIVE");
   var data = sheet.getDataRange().getValues();
   var headers = data[0];
 
@@ -15,14 +13,22 @@ function getStudentsFromTENTATIVESheet() {
   for (var i = 1; i < data.length; i++) {
     var studentId = data[i][headers.indexOf("STUDENT ID")];
 
+    // Create an object to hold the student's data
     var studentData = {};
     for (var j = 0; j < headers.length; j++) {
       studentData[headers[j]] = data[i][j];
     }
 
-    allStudentsMap1.set(studentId, studentData);
+    // If the student ID already exists, push the new studentData into the array
+    if (allStudentsMap1.has(studentId)) {
+      allStudentsMap1.get(studentId).push(studentData);
+    } else {
+      // Otherwise, create a new array with the studentData
+      allStudentsMap1.set(studentId, [studentData]);
+    }
   }
 
-  // Logger.log("TENTATIVE Data: " + JSON.stringify([...allStudentsMap]));
+  // Logger.log("TENTATIVE Data: " + JSON.stringify([...allStudentsMap1]));
   return allStudentsMap1;
 }
+
