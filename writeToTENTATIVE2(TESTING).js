5806ced6e29df7e8b60458813782225350e8ade7
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-10-03 14:58:18 -0500

    Added logic to be able to pull data and add it to the output

--- writeToTENTATIVE2(TESTING).js
+++ writeToTENTATIVE2(TESTING).js
@@ -1,9 +1,12 @@
 /**
  * Writes the data from updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap to the "TENTATIVE2(TESTING)" sheet.
  */
-function writeToTENTATIVE2_TESTINGSheet(updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap) {
-  const activeSheet =
-    SpreadsheetApp.getActiveSpreadsheet().getSheetByName("TENTATIVE2(TESTING)");
+function writeToTENTATIVE2_TESTINGSheet(
+  updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap,
+) {
+  const activeSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
+    "TENTATIVE2(TESTING)",
+  );
 
   // Clear existing data from Row 2 downwards
   const lastRow = activeSheet.getLastRow();
@@ -15,12 +18,24 @@ function writeToTENTATIVE2_TESTINGSheet(updatedUpdatedUpdatedUdatedUpdatedUpdate
 
   // Check if the input is defined and log its content
   if (!updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap) {
-    console.error("updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap is undefined.");
+    console.error(
+      "updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap is undefined.",
+    );
     return; // Exit the function if it's undefined
   }
 
-  if (typeof updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap !== "object" || !("forEach" in updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap)) {
-    console.error("leftJoinResults is not iterable:", updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap);
+  if (
+    typeof updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap !==
+      "object" ||
+    !(
+      "forEach" in
+      updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap
+    )
+  ) {
+    console.error(
+      "leftJoinResults is not iterable:",
+      updatedUpdatedUpdatedUdatedUpdatedUpdatedUpdatedActiveStudentDataMap,
+    );
     return; // Exit the function if it's not iterable
   }
 
@@ -41,7 +56,7 @@ function writeToTENTATIVE2_TESTINGSheet(updatedUpdatedUpdatedUdatedUpdatedUpdate
 
   /**
    * Calculates the expected withdraw date for the student.
-   * 
+   *
    * @param {formattedEntryDate} - The date student registered at DAEP.
    * @param {placementDays} - The number of placement days.
    * @param {daysInEnrl} - The number of days the student has been enrolled at DAEP.
@@ -49,8 +64,12 @@ function writeToTENTATIVE2_TESTINGSheet(updatedUpdatedUpdatedUdatedUpdatedUpdate
    * @returns
    * TODO: Is this function needed? Can we just call NAHS_EXPECTED_WITHDRAW_DATE directly?
    */
-  function calculateExpectedWithdrawDate(formattedEntryDate, placementDays, daysInEnrl, daysInAtt) {
-
+  function calculateExpectedWithdrawDate(
+    formattedEntryDate,
+    placementDays,
+    daysInEnrl,
+    daysInAtt,
+  ) {
     // Validate dates
     if (!formattedEntryDate || isNaN(new Date(formattedEntryDate).getTime())) {
       console.error("Invalid formatted entry date:", formattedEntryDate);
@@ -114,7 +133,7 @@ function writeToTENTATIVE2_TESTINGSheet(updatedUpdatedUpdatedUdatedUpdatedUpdate
 
       // Continue with the rest of your logic
       const formattedEntryDate = formatDateToMMDDYYYY(entryData);
-      const gradeString = tentativeArray[0]["GRADE"]; // TODO: Is this variable needed? If not, remove it
+      // const gradeString = tentativeArray[0]["GRADE"]; // TODO: Is this variable needed? If not, remove it
       // const formattedGrade =
       //   typeof gradeString === "string" && gradeString
       //     ? gradeString.split("-")[0].trim().replace(/^0+/, "")
@@ -136,9 +155,7 @@ function writeToTENTATIVE2_TESTINGSheet(updatedUpdatedUpdatedUdatedUpdatedUpdate
       const placementDays = registrationArray
         ? registrationArray[0]["Placement Days"]
         : null;
-      const daysInAttendance = attendanceArray
-        ? attendanceArray[0][4]
-        : null;
+      const daysInAttendance = attendanceArray ? attendanceArray[0][4] : null;
       const estimatedDaysLeft = // TODO: Is this variable needed? If not, remove it
         placementDays !== null && daysInAttendance !== null
           ? placementDays - daysInAttendance
@@ -147,15 +164,24 @@ function writeToTENTATIVE2_TESTINGSheet(updatedUpdatedUpdatedUdatedUpdatedUpdate
       const daysInEnrl = attendanceArray ? attendanceArray[0][5] : 0;
 
       // Calculate Expected Withdraw Date
-      const estimatedExitDay = calculateExpectedWithdrawDate(formattedEntryDate, placementDays, daysInEnrl, daysInAttendance); // Call the function to get the expected withdraw date
+      const estimatedExitDay = calculateExpectedWithdrawDate(
+        formattedEntryDate,
+        placementDays,
+        daysInEnrl,
+        daysInAttendance,
+      ); // Call the function to get the expected withdraw date
 
       const fullName =
         studentData["Entry_Withdrawal"][0]["Student Name(Last, First)"];
       const [backUpLastName, backUpFirstName] = fullName
         .split(",")
         .map((name) => name.trim());
-      const lastName = tentativeArray ? tentativeArray[0]["LAST"] : backUpLastName;
-      const firstName = tentativeArray ? tentativeArray[0]["FIRST"] : backUpFirstName;
+      const lastName = tentativeArray
+        ? tentativeArray[0]["LAST"]
+        : backUpLastName;
+      const firstName = tentativeArray
+        ? tentativeArray[0]["FIRST"]
+        : backUpFirstName;
 
       /**
        * This is a helper function that checks if the
@@ -185,8 +211,7 @@ function writeToTENTATIVE2_TESTINGSheet(updatedUpdatedUpdatedUdatedUpdatedUpdate
 
       const educationalFactors =
         registrationArray[0] &&
-        registrationArray[0]["Educational Factors"] !==
-          undefined
+        registrationArray[0]["Educational Factors"] !== undefined
           ? registrationArray[0]["Educational Factors"]
           : null;
 
@@ -214,82 +239,212 @@ function writeToTENTATIVE2_TESTINGSheet(updatedUpdatedUpdatedUdatedUpdatedUpdate
         ? tentativeArray[0]["Document Merge Status - Transition Letter"]
         : null;
 
+      /**
+       * Updates the teacherInput object with values from formResponses1Array.
+       *
+       * @param {Object} teacherInput - The teacher input object to be updated.
+       * @param {Array<Object>} formResponses1Array - The array of form responses.
+       */
+      function updateTeacherInput(teacherInput, formResponses1Array) {
+        // Iterate through each period in the teacherInput object
+        for (const period in teacherInput) {
+          if (teacherInput.hasOwnProperty(period)) {
+            // Find the matching entry in the formResponsesArray
+            const matchingEntry = formResponses1Array.find(
+              (response) =>
+                response["What period do you have this student?"] === period,
+            );
+
+            // If a matching entry is found, update the teacherInput object
+            if (matchingEntry) {
+              teacherInput[period][
+                "How would you assess this student's academic growth?"
+              ] =
+                matchingEntry[
+                  "How would you assess this student's academic growth?"
+                ] || "";
+              teacherInput[period]["Academic and Behavioral Progress Notes"] =
+                matchingEntry["Academic and Behavioral Progress Notes"] || "";
+              teacherInput[period]["Teacher Name"] =
+                matchingEntry["Teacher"] || "";
+              teacherInput[period]["Course Title"] =
+                matchingEntry["Course Title"] || "";
+            }
+          }
+        }
+      }
+
+      const teacherInput = {
+        "1st": {
+          "Course Title": "",
+          "Teacher Name": "",
+          "Transfer Grade": "",
+          "Current Grade": "",
+          "How would you assess this student's academic growth?": "",
+          "Academic and Behavioral Progress Notes": "",
+        },
+        "2nd": {
+          "Course Title": "",
+          "Teacher Name": "",
+          "Transfer Grade": "",
+          "Current Grade": "",
+          "How would you assess this student's academic progress?": "",
+          "Academic and Behavioral Progress Notes": "",
+        },
+        "3rd": {
+          "Course Title": "",
+          "Teacher Name": "",
+          "Transfer Grade": "",
+          "Current Grade": "",
+          "How would you assess this student's academic progress?": "",
+          "Academic and Behavioral Progress Notes": "",
+        },
+        "4th": {
+          "Course Title": "",
+          "Teacher Name": "",
+          "Transfer Grade": "",
+          "Current Grade": "",
+          "How would you assess this student's academic progress?": "",
+          "Academic and Behavioral Progress Notes": "",
+        },
+        "5th": {
+          "Course Title": "",
+          "Teacher Name": "",
+          "Transfer Grade": "",
+          "Current Grade": "",
+          "How would you assess this student's academic progress?": "",
+          "Academic and Behavioral Progress Notes": "",
+        },
+        "6th": {
+          "Course Title": "",
+          "Teacher Name": "",
+          "Transfer Grade": "",
+          "Current Grade": "",
+          "How would you assess this student's academic progress?": "",
+          "Academic and Behavioral Progress Notes": "",
+        },
+        "7th": {
+          "Course Title": "",
+          "Teacher Name": "",
+          "Transfer Grade": "",
+          "Current Grade": "",
+          "How would you assess this student's progress?": "",
+          "Academic and Behavioral Progress Notes": "",
+        },
+        "8th": {
+          "Course Title": "",
+          "Teacher Name": "",
+          "Transfer Grade": "",
+          "Current Grade": "",
+          "How would you assess this student's progress?": "",
+          "Academic and Behavioral Progress Notes": "",
+        },
+        "Special Education": {
+          "Case Manager": "",
+          "What accommodations seem to work well with this student to help them be successful?":
+            "",
+          "What are the student's strengths, as far as behavior?": "",
+          "What are the student's needs, as far as behavior?": "",
+          "What are the student's needs, as far as functional skills?": "",
+          "Please add any other comments or concerns here:": "",
+        },
+      };
+
+      // Check if formResponses1Array is null before calling updateTeacherInput
+      if (formResponses1Array !== null) {
+        updateTeacherInput(teacherInput, formResponses1Array);
+      } else {
+        console.log(
+          "formResponses1Array is null. Skipping updateTeacherInput.",
+        );
+      }
+
       // Extract the needed data fields in the specified order
       outputData.push([
-        tentativeArray
-          ? tentativeArray[0]["DATE ADDED TO SPREADSHEET"]
-          : null, // TODO: ADD A FUNCTION TO CHECK IF THIS VALUE IS BLANK, IF IT IS THEN ADD THE CURRENT DATE & TIME IF IT ISN'T BLANK THEN BRING IN THE DATA
-        lastName, // "LAST"
-        firstName, // "FIRST"
-        studentId
-          ? studentId
-          : null, // "STUDENT ID",
-        tentativeArray[0]
-          ? tentativeArray[0]["GRADE"]
-          : null, // "GRADE",
+        tentativeArray ? formatDateToMMDDYYYY(tentativeArray[0]["DATE ADDED TO SPREADSHEET"]) : null, // TODO: ADD A FUNCTION TO CHECK IF THIS VALUE IS BLANK, IF IT IS THEN ADD THE CURRENT DATE & TIME IF IT ISN'T BLANK THEN BRING IN THE DATA
+        lastName,
+        firstName,
+        studentId ? studentId : null,
+        tentativeArray[0] ? tentativeArray[0]["GRADE"] : null,
 
-        // "1st Period - Course Title",
-        // "1st Period - Teacher Name",
-        // "1st Period - Transfer Grade",
-        // "1st Period - Current Grade",
-        // "1st Period - How would you assess this student's academic growth?",
-        // "1st Period - Academic and Behavioral Progress Notes",
+        teacherInput["1st"]["Course Title"], // "1st Period - Course Title",
+        teacherInput["1st"]["Teacher Name"], // "1st Period - Teacher Name",
+        "", // "1st Period - Transfer Grade",
+        "", // "1st Period - Current Grade",
+        teacherInput["1st"][
+          "How would you assess this student's academic growth?"
+        ],
+        teacherInput["1st"]["Academic and Behavioral Progress Notes"],
 
-        // "2nd Period - Course Title",
-        // "2nd Period - Teacher Name",
-        // "2nd Period - Transfer Grade",
-        // "2nd Period - Current Grade",
-        // "2nd Period - How would you assess this student's academic progress?",
-        // "2nd Period - Academic and Behavioral Progress Notes",
+        teacherInput["2nd"]["Course Title"], // "2nd Period - Course Title",
+        teacherInput["2nd"]["Teacher Name"], // "2nd Period - Teacher Name",
+        "", // "2nd Period - Transfer Grade",
+        "", // "2nd Period - Current Grade",
+        teacherInput["2nd"][
+          "How would you assess this student's academic growth?"
+        ],
+        teacherInput["2nd"]["Academic and Behavioral Progress Notes"],
 
-        // "3rd Period - Course Title",
-        // "3rd Period - Teacher Name",
-        // "3rd Period - Transfer Grade",
-        // "3rd Period - Current Grade",
-        // "3rd Period - How would you assess this student's academic progress?",
-        // "3rd Period - Academic and Behavioral Progress Notes",
+        teacherInput["3rd"]["Course Title"], // "3rd Period - Course Title",
+        teacherInput["3rd"]["Teacher Name"], // "3rd Period - Teacher Name",
+        "", // "3rd Period - Transfer Grade",
+        "", // "3rd Period - Current Grade",
+        teacherInput["3rd"][
+          "How would you assess this student's academic growth?"
+        ],
+        teacherInput["3rd"]["Academic and Behavioral Progress Notes"],
 
-        // "4th Period - Course Title",
-        // "4th Period - Teacher Name",
-        // "4th Period - Transfer Grade",
-        // "4th Period - Current Grade",
-        // "4th Period - How would you assess this student's academic progress?",
-        // "4th Period - Academic and Behavioral Progress Notes",
+        teacherInput["4th"]["Course Title"], // "4th Period - Course Title",
+        teacherInput["4th"]["Teacher Name"], // "4th Period - Teacher Name",
+        "", // "4th Period - Transfer Grade",
+        "", // "4th Period - Current Grade",
+        teacherInput["4th"][
+          "How would you assess this student's academic growth?"
+        ],
+        teacherInput["4th"]["Academic and Behavioral Progress Notes"],
 
-        // "5th Period - Course Title",
-        // "5th Period - Teacher Name",
-        // "5th Period - Transfer Grade",
-        // "5th Period - Current Grade",
-        // "5th Period - How would you assess this student's academic progress?",
-        // "5th Period - Academic and Behavioral Progress Notes",
+        teacherInput["5th"]["Course Title"], // "5th Period - Course Title",
+        teacherInput["5th"]["Teacher Name"], // "5th Period - Teacher Name",
+        "", // "5th Period - Transfer Grade",
+        "", // "5th Period - Current Grade",
+        teacherInput["5th"][
+          "How would you assess this student's academic growth?"
+        ],
+        teacherInput["5th"]["Academic and Behavioral Progress Notes"],
 
-        // "6th Period - Course Title",
-        // "6th Period - Teacher Name",
-        // "6th Period - Transfer Grade",
-        // "6th Period - Current Grade",
-        // "6th Period - How would you assess this student's academic progress?",
-        // "6th Period - Academic and Behavioral Progress Notes",
+        teacherInput["6th"]["Course Title"], // "6th Period - Course Title",
+        teacherInput["6th"]["Teacher Name"], // "6th Period - Teacher Name",
+        "", // "6th Period - Transfer Grade",
+        "", // "6th Period - Current Grade",
+        teacherInput["6th"][
+          "How would you assess this student's academic growth?"
+        ],
+        teacherInput["6th"]["Academic and Behavioral Progress Notes"],
 
-        // "7th Period - Course Title",
-        // "7th Period - Teacher Name",
-        // "7th Period - Transfer Grade",
-        // "7th Period - Current Grade",
-        // "7th Period - How would you assess this student's progress?",
-        // "7th Period - Academic and Behavioral Progress Notes",
+        teacherInput["7th"]["Course Title"], // "7th Period - Course Title",
+        teacherInput["7th"]["Teacher Name"], // "7th Period - Teacher Name",
+        "", // "7th Period - Transfer Grade",
+        "", // "7th Period - Current Grade",
+        teacherInput["7th"][
+          "How would you assess this student's academic growth?"
+        ],
+        teacherInput["7th"]["Academic and Behavioral Progress Notes"],
 
-        // "8th Period - Course Title",
-        // "8th Period - Teacher Name",
-        // "8th Period - Transfer Grade",
-        // "8th Period - Current Grade",
-        // "8th Period - How would you assess this student's progress?",
-        // "8th Period - Academic and Behavioral Progress Notes",
+        teacherInput["8th"]["Course Title"], // "8th Period - Course Title",
+        teacherInput["8th"]["Teacher Name"], // "8th Period - Teacher Name",
+        "", // "8th Period - Transfer Grade",
+        "", // "8th Period - Current Grade",
+        teacherInput["8th"][
+          "How would you assess this student's academic growth?"
+        ],
+        teacherInput["8th"]["Academic and Behavioral Progress Notes"],
 
-        // "SE - Special Education Case Manager",
-        // "SE - What accommodations seem to work well with this student to help them be successful?",
-        // "SE - What are the student's strengths, as far as behavior?",
-        // "SE - What are the student's needs, as far as behavior?  (Pick behavior having most effect on his/her ability to be successful in class.  If there are no concerns, note that.)",
-        // "SE - What are the student's needs, as far as functional skills?  (Include daily living skills, fine/gross motor skills, organizational skills, self-advocacy, attendance, etc.)",
-        // "SE - Please add any other comments or concerns here:",
+        "", // "SE - Special Education Case Manager",
+        "", // "SE - What accommodations seem to work well with this student to help them be successful?",
+        "", // "SE - What are the student's strengths, as far as behavior?",
+        "", // "SE - What are the student's needs, as far as behavior?  (Pick behavior having most effect on his/her ability to be successful in class.  If there are no concerns, note that.)",
+        "", // "SE - What are the student's needs, as far as functional skills?  (Include daily living skills, fine/gross motor skills, organizational skills, self-advocacy, attendance, etc.)",
+        "", // "SE - Please add any other comments or concerns here:",
 
         studentData.Registrations_SY_24_25
           ? studentData.Registrations_SY_24_25[0]["Home Campus"]
