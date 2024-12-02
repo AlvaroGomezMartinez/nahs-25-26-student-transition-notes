31294bb3d949e93d55e148beaac9033b98278ef8
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-12-02 17:01:10 -0600

    Fix, so that emails are sent from Version 2

--- reminderEmails.js
+++ reminderEmails.js
@@ -1,92 +1,102 @@
-/**********************************************************************************
- *                         nahs-transition-project-24-25a                         *
- *                                                                                *
- * The sendEmailsForToday function below references the object returned by        *
- * registrationsData which is found in the sourceScript file in this project.     *
- *                                                                                *
- * sendEmailsForToday looks at the tenDaysMark date for each student and if the   *
- * date is today, then it sends an email to the teachers in emailRecipients with  *
- * a list of the students at the 10 Day Mark.                                     *
- *                                                                                *
- * sendEmailsForToday has five triggers set to run it every Monday, Tuesday,      *
- * Wednesday, Thursday, and Friday on Zina Gonzales' (social worker at NAHS)      *
- * gmail account so the emails to the teachers are recieved from her.             *
- *                                                                                *
- * Point of contact: Alvaro Gomez                                                 *
- *                   Academic Technology Coach                                    *
- *                   alvaro.gomez@nisd.net                                        *
- *                   Office: +1-210-397-9408                                      *
- *                   Mobile: +1-210-363-1577                                      *
- *                                                                                *
- * Latest update: 09/03/24                                                        *
- **********************************************************************************/
+/**
+ *
+ * The sendEmailsForToday function below references the object returned by the
+ * getStudentsFromTENTATIVESheet() which is found in the loadTENTATIVEVersion2.gs
+ * file in this project.
+ *
+ * sendEmailsForToday looks at the tenDaysMark date for each student and if the
+ * date is today, then it sends an email to the teachers in emailRecipients with
+ * a list of the students at the 10 Day Mark.
+ *
+ * sendEmailsForToday has five triggers set to run it every Monday, Tuesday,
+ * Wednesday, Thursday, and Friday on Zina Gonzales' (social worker at NAHS)
+ * gmail account so the emails to the teachers are recieved from her.
+ *
+ * Point of contact: Alvaro Gomez
+ *                   Academic Technology Coach
+ *                   alvaro.gomez@nisd.net
+ *                   Office: +1-210-397-9408
+ *                   Mobile: +1-210-363-1577
+ *
+ * Latest update: 11/07/24
+ */
 
+/**
+ * Sends an email notification to specific recipients with a list of students 
+ * who have been enrolled for 10 days at NAHS. The function checks if each 
+ * student's 10-day mark matches today's date and compiles a list of such students.
+ * If matches are found, an email is sent with an action item for teachers to provide 
+ * input on students' academic and behavioral progress.
+ */
 function sendEmailsForToday() {
   let today = Utilities.formatDate(
     new Date(),
     Session.getScriptTimeZone(),
     "yyyy-MM-dd"
   );
-  // let today = '2024-09-07' // This is for testing and sending emails out manually. Set today's date in the variable. When using this, don't forget to turn it back off.
-  let dataObjects = registrationsData();
+  // let today = '2024-11-07' // This is for testing and sending emails out manually. Set today's date in the variable. When using this, don't forget to turn it back off.
+
+  let dataObjects;
+
+  try {
+    dataObjects = getStudentsFromTENTATIVESheet();
+  } catch (error) {
+    Logger.log("Log 44: Error calling getStudentsFromTENTATIVESheet " + error);
+    return;
+  }
+
+  // Check if dataObjects is defined and not empty
+  if (!dataObjects || dataObjects.size === 0) {
+    Logger.log(
+      "Line 51: No data found in dataObjects or dataObjects is undefined."
+    );
+    return;
+  }
+
   let studentsForToday = [];
 
-  for (let i = 0; i < dataObjects.length; i++) {
-    let dataObject = dataObjects[i];
-    let tenDaysMark = dataObject["10 Days Mark"];
+  try {
+    for (let [studentID, dataArray] of dataObjects.entries()) {
+      if (dataArray && dataArray[0] && dataArray[0]["FIRST DAY OF AEP"]) {
+        let startDate = new Date(dataArray[0]["FIRST DAY OF AEP"]);
+        let tenDaysMark = addWorkdays(startDate, 10, holidayDates);
+        let formattedTenDaysMark = Utilities.formatDate(
+          tenDaysMark,
+          Session.getScriptTimeZone(),
+          "yyyy-MM-dd"
+        );
 
-    if (tenDaysMark === today) {
-      let lastName = dataObject["Student Last Name"];
-      let firstName = dataObject["Student First Name"];
-      let studentID = dataObject["Student ID"];
-      let studentGrade = dataObject["Grade"];
+        if (formattedTenDaysMark === today) {
+          let lastName = dataArray[0]["LAST"];
+          let firstName = dataArray[0]["FIRST"];
+          let studentID = dataArray[0]["STUDENT ID"];
+          let studentGrade = dataArray[0]["GRADE"];
 
-      studentsForToday.push(
-        `${lastName}, ${firstName} (${studentID}), Grade: ${studentGrade}`
-      );
+          studentsForToday.push(
+            `${lastName}, ${firstName} (${studentID}), Grade: ${studentGrade}`
+          );
+        }
+      } else {
+        Logger.log(
+          `Line 81: Data structure issue for Student ID ${studentID}: "FIRST DAY OF AEP" not found`
+        );
+      }
     }
+  } catch (error) {
+    Logger.log("Line 86: Error within for loop: " + error);
+    return;
   }
 
   if (studentsForToday.length > 0) {
-    let holidayDates = [
-      "2024-09-02",
-      "2024-10-14",
-      "2024-11-05",
-      "2024-11-25",
-      "2024-11-26",
-      "2024-11-27",
-      "2024-11-28",
-      "2024-11-29",
-      "2024-12-23",
-      "2024-12-24",
-      "2024-12-25",
-      "2024-12-26",
-      "2024-12-27",
-      "2024-12-30",
-      "2024-12-31",
-      "2025-01-01",
-      "2025-01-02",
-      "2025-01-03",
-      "2025-01-06",
-      "2025-01-20",
-      "2025-02-17",
-      "2025-03-10",
-      "2025-03-11",
-      "2025-03-12",
-      "2025-03-13",
-      "2025-03-14",
-      "2025-04-18",
-      "2025-04-21",
-      "2025-05-02",
-      "2025-05-26",
-    ];
-
-    // Function to check if a date is a weekend (Saturday or Sunday)
+    /**
+     * Helper function to check if a given date falls on a weekend.
+     * @param {Date} date - The date to check.
+     * @return {boolean} True if the date is a weekend, otherwise false.
+     */
     function isWeekend(date) {
       return date.getDay() === 0 || date.getDay() === 6; // 0 represents Sunday, 6 represents Saturday
     }
 
-    // Calculate the due date
     let dueDate = new Date(today);
 
     // Loop to add two workdays (excluding weekends and holidays)
@@ -94,7 +104,6 @@ function sendEmailsForToday() {
     while (workdaysAdded < 2) {
       dueDate.setDate(dueDate.getDate() + 1);
 
-      // Check if the new date is not a weekend and not in the holidayDates array
       if (
         !isWeekend(dueDate) &&
         !holidayDates.includes(
@@ -109,7 +118,6 @@ function sendEmailsForToday() {
       }
     }
 
-    // Format the due date
     let formattedDueDate = Utilities.formatDate(
       dueDate,
       Session.getScriptTimeZone(),
@@ -119,11 +127,10 @@ function sendEmailsForToday() {
       "Transition Reminder: Today's List of Students with 10 Days at NAHS";
     let formLink = "https://forms.gle/1NirWqZkvcABGgYc9";
     let body = `NAHS Teachers,\n\nBelow is today's list of students that have been enrolled for 10 days at NAHS:\n\n${studentsForToday.join(
-      "\n",
+      "\n"
     )}\n\nACTION ITEM (Due by end of day, ${formattedDueDate}): If you have one of these students on your roster, please go to: ${formLink} and provide your input on their academic growth and behavioral progress.\n****REMINDER***a\nWhen inputting the period on the form, select the period that is listed on the student's schedule, the one you enter their attendance with. \n\nThank you`;
 
-    // The emailRecipients array below is used for testing.
-    // let emailRecipients = ['alvaro.gomez@nisd.net'];
+    // let emailRecipients = ['alvaro.gomez@nisd.net']; // This commented out version of emailRecipients is used for testing.
     let emailRecipients = [
       "marco.ayala@nisd.net",
       "alita.barrera@nisd.net",
